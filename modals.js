bae4b478cce4a9800a350b51d77ad695ac9b099b
Max <hallsetts@gmail.com>
2017-03-01 18:53:48 -0500

    update

--- modals.js
+++ modals.js
@@ -1,11 +1,10 @@
- //Modal Image Gallery
-function onClick(element) {
-    document.getElementById("img01").src = element.src;
-    document.getElementById("modal01").style.display = "block";
-    var captionText = document.getElementById("Text");
-    captionText.innerHTML = element.alt;
-}
-
+// //Modal Image Gallery
+//function onClick(element) {
+//    document.getElementById("img01").src = element.src;
+//    document.getElementById("modal01").style.display = "block";
+//    var captionText = document.getElementById("Text");
+//    captionText.innerHTML = element.alt;
+//}
 
 //Modal LightBox
 function openModal(id) {
@@ -28,9 +27,7 @@ function closeModal() {
 }
 
 var slideIndex = 1;
-var slideIndex = 1;
 showDivs(slideIndex);
-showDivs2(slideIndex);
 
 function plusDivs(n) {
     showDivs(slideIndex += n);
@@ -40,41 +37,11 @@ function currentDiv(n) {
     showDivs(slideIndex = n);
 }
 
-function plusDivs2(n) {
-    showDivs2(slideIndex += n);
-}
-
-function currentDiv2(n) {
-    showDivs2(slideIndex = n);
-}
-
 function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("demo");
     var captionText = document.getElementById("caption");
-    if (n > x.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = x.length
-    }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
-    }
-    x[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " w3-opacity-off";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
-}
-
-function showDivs2(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides2");
-    var dots = document.getElementsByClassName("demo2");
-    var captionText = document.getElementById("caption2");
     if (n > x.length) {
         slideIndex = 1
     }
